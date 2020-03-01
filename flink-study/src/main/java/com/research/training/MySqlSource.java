35f9c426352ff8ef5e8a37b2593821dd2a1e243a
collabH <h1261109615@qq.com>
2020-03-02 00:08:45 +0800

    feat(training): es sink kafka source mysql sink
    将mysql数据和kafka数据连接处理过滤后输出到es中

---
+++ flink-study/src/main/java/com/research/training/MySqlSource.java
@@ -0,0 +1,57 @@
+package com.research.training;
+
+import com.google.common.collect.Maps;
+import com.mysql.jdbc.Driver;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+
+/**
+ * @fileName: MySqlSource.java
+ * @description: MySqlSource.java类说明
+ * @author: by echo huang
+ * @date: 2020-03-01 16:10
+ */
+public class MySqlSource extends RichParallelSourceFunction<Map<String, String>> {
+    private Connection connection;
+    private PreparedStatement ps;
+
+    @Override
+    public void run(SourceContext<Map<String, String>> ctx) throws Exception {
+        Map<String, String> data = Maps.newHashMap();
+        ResultSet resultSet = ps.getResultSet();
+        while (resultSet.next()) {
+            data.put(resultSet.getString("domain"), resultSet.getString("user_id"));
+        }
+        ctx.collect(data);
+    }
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        Class.forName(Driver.class.getName());
+        connection = DriverManager.getConnection("jdbc:mysql:///sandbox-monitor", "root", "root");
+        ps = connection.prepareStatement("select  user_id,domain from user_domain_config");
+        ps.execute();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+        if (ps != null) {
+            ps.close();
+        }
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+}
--- flink-study/src/main/java/com/research/training/MysqlFlinkKafkaConsumer.java
+++ flink-study/src/main/java/com/research/training/MysqlFlinkKafkaConsumer.java
@@ -8,6 +8,7 @@ import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DateUtils;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.java.tuple.Tuple;
@@ -17,14 +18,21 @@ import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoFlatMapFunction;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.connectors.elasticsearch.ElasticsearchSinkFunction;
+import org.apache.flink.streaming.connectors.elasticsearch.RequestIndexer;
 import org.apache.flink.streaming.connectors.elasticsearch7.ElasticsearchSink;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.Requests;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.atomic.LongAdder;
 
@@ -35,13 +43,15 @@ import java.util.concurrent.atomic.LongAdder;
  * @date: 2020-02-28 11:26
  */
 @Slf4j
-public class TestFlinkKafkaConsumer {
+public class MysqlFlinkKafkaConsumer {
     public static void main(String[] args) {
+        //elasticSearch sink
+        ElasticsearchSink.Builder<Tuple4<String, String, String, Long>> builder = new ElasticsearchSink.Builder<>(Lists.newArrayList(new HttpHost("localhost", 9200)),
+                new ElasticsearchSink2());
+        builder.setBulkFlushMaxActions(1);
+        //mysql source
+        DataStreamSource<Map<String, String>> mysqlSource = StreamExecutionEnvironment.getExecutionEnvironment().addSource(new MySqlSource());
 
-        ElasticsearchSink.Builder<Tuple3<String, String, Long>> esSink = new ElasticsearchSink.Builder<>(
-                Lists.newArrayList(new HttpHost("localhost", 9200)),
-                new com.research.training.ElasticsearchSink());
-        esSink.setBulkFlushMaxActions(1);
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
         props.setProperty("group.id", "test");
@@ -85,14 +95,51 @@ public class TestFlinkKafkaConsumer {
                         input.spliterator().forEachRemaining(tuple -> sum.add(tuple.f2));
                         out.collect(new Tuple3<>(window.getStart() + "  " + window.getEnd(), domain, sum.longValue()));
                     }
-                })
-                .addSink(esSink.build());
+                }).connect(mysqlSource)
+                .flatMap(new CoFlatMapFunction<Tuple3<String, String, Long>, Map<String, String>, Tuple4<String, String, String, Long>>() {
+                    private Map<String, String> map;
+
+                    //log
+                    @Override
+                    public void flatMap1(Tuple3<String, String, Long> value, Collector<Tuple4<String, String, String, Long>> out) throws Exception {
+                        String userId = map.get(value.f1);
+                        out.collect(new Tuple4<>(userId, value.f0, value.f1, value.f2));
+                    }
+
+                    //mysql
+                    @Override
+                    public void flatMap2(Map<String, String> value, Collector<Tuple4<String, String, String, Long>> out) throws Exception {
+                        map = value;
+                    }
+                }).addSink(builder.build());
 
 
         try {
             env.execute("flink kafka consumer");
         } catch (Exception e) {
             System.out.println("启动失败");
+        }
+    }
+
+    static class ElasticsearchSink2 implements ElasticsearchSinkFunction<Tuple4<String, String, String, Long>> {
+
+
+        private IndexRequest createIndexRequest(Tuple4<String, String, String, Long> input) {
+            Map<String, Object> json = new HashMap<>();
+            json.put("userId", input.f0);
+            json.put("time", input.f1);
+            json.put("domain", input.f2);
+            json.put("triffic_count", input.f3);
+
+            return Requests.indexRequest()
+                    .index("my-index2")
+                    .type("_doc")
+                    .source(json);
+        }
+
+        @Override
+        public void process(Tuple4<String, String, String, Long> input, RuntimeContext runtimeContext, RequestIndexer requestIndexer) {
+            requestIndexer.add(createIndexRequest(input));
         }
     }
 }
