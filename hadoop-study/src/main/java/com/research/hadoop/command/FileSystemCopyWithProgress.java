667a7536b2454d8ac4788ceeae92d425c0c678fd
collabH <h1261109615@qq.com>
2020-03-18 01:26:57 +0800

    feat: hadoop fileSystem operator
    hadoop文件系统操作

---
+++ hadoop-study/src/main/java/com/research/hadoop/command/FileSystemCopyWithProgress.java
@@ -0,0 +1,41 @@
+package com.research.hadoop.command;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.net.URI;
+
+/**
+ * @fileName: FileSystemCopyWithProgress.java
+ * @description: FileSystemCopyWithProgress.java类说明
+ * @author: by echo huang
+ * @date: 2020-03-17 17:00
+ */
+public class FileSystemCopyWithProgress {
+    public static void main(String[] args) throws Exception {
+        append();
+    }
+
+    private static void create() throws Exception {
+        String path = FileSystemCopyWithProgress.class.getClassLoader().getResource("word.txt").getPath();
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(path));
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create("hdfs://localhost:8020/word.txt"), conf);
+        FSDataOutputStream out = fs.create(new Path("hdfs://localhost:8020/word.txt"), () -> System.out.println("datanode 回馈"));
+        IOUtils.copyBytes(in, out, 4096, true);
+    }
+
+    private static void append() throws Exception {
+        String path = FileSystemCopyWithProgress.class.getClassLoader().getResource("word.txt").getPath();
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(path));
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create("hdfs://localhost:8020/word.txt"), conf);
+        FSDataOutputStream out = fs.append(new Path("hdfs://localhost:8020/word.txt"),4096, () -> System.out.println("datanode 回馈"));
+        IOUtils.copyBytes(in, out, 4096, true);
+    }
+}
