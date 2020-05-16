b6b5573250326bfd6da5aa9b1a7c02a59a21b785
collabH <h1261109615@qq.com>
2020-05-16 16:02:57 +0800

    feat: study state function

---
+++ stateful-function/src/main/java/org/stateful/function/GreetFunction.java
@@ -0,0 +1,26 @@
+package org.stateful.function;
+
+import org.apache.flink.statefun.sdk.Context;
+import org.apache.flink.statefun.sdk.StatefulFunction;
+import org.apache.flink.statefun.sdk.io.EgressIdentifier;
+import org.stateful.function.domain.GreetRequest;
+import org.stateful.function.domain.GreetResponse;
+
+/**
+ * @fileName: GreetFunction.java
+ * @description: 状态函数
+ * @author: by echo huang
+ * @date: 2020-05-06 10:44
+ */
+public class GreetFunction implements StatefulFunction {
+    @Override
+    public void invoke(Context context, Object input) {
+        GreetRequest request = (GreetRequest) input;
+        GreetResponse response = GreetResponse.newBuilder()
+                .setWho(request.getWho())
+                .setGreeting("Hello " + request.getWho())
+                .build();
+
+        context.send(new EgressIdentifier<>("state", "test", GreetResponse.class), response);
+    }
+}
