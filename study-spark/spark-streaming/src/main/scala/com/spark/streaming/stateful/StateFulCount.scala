0be12ce46d0d28783fb2acd4b69f7ef564b3fbe8
Shiming Huang <h1261109615@qq.com>
2020-07-03 00:29:43 +0800

    feat: add sparkStreaming demo

---
+++ study-spark/spark-streaming/src/main/scala/com/spark/streaming/stateful/StateFulCount.scala
@@ -0,0 +1,11 @@
+package com.spark.streaming.quickstart.stateful
+
+/**
+  * @fileName: StateFulCount.java
+  * @description: StateFulCount.java类说明
+  * @author: by echo huang
+  * @date: 2020-07-02 13:42
+  */
+class StateFulCount {
+
+}
