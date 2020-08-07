7c706cc0d4ee2e58fcdd942d0d5ee6bff91e7eb4
Shiming Huang <h1261109615@qq.com>
2020-08-07 23:44:51 +0800

    feat: add HBase DML Scan&Delete

--- research-hbase/src/main/java/org/reasearch/hbase/api/HBaseClient.java
+++ research-hbase/src/main/java/org/reasearch/hbase/api/HBaseClient.java
@@ -11,6 +11,7 @@ import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * @fileName: HBaseClient.java
@@ -93,6 +94,69 @@ public class HBaseClient {
             System.out.println(Bytes.toString(CellUtil.cloneQualifier(cell)));
             System.out.println(Bytes.toString(CellUtil.cloneValue(cell)));
         }
+    }
+
+    /**
+     * scan
+     *
+     * @param tableName
+     * @throws IOException
+     */
+    public static void scanTable(String tableName) throws IOException {
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        Scan scan = new Scan();
+
+        Iterator<Result> iterator = table.getScanner(scan).iterator();
+        while (iterator.hasNext()) {
+            Result next = iterator.next();
+            for (Cell cell : next.rawCells()) {
+                System.out.println(Bytes.toString(CellUtil.cloneRow(cell)));
+                System.out.println(Bytes.toString(CellUtil.cloneFamily(cell)));
+                System.out.println(Bytes.toString(CellUtil.cloneQualifier(cell)));
+                System.out.println(Bytes.toString(CellUtil.cloneValue(cell)));
+            }
+        }
+    }
+
+    /**
+     * delete
+     *
+     * @param tableName
+     * @param rowKey
+     * @param cf
+     * @param cn
+     * @throws IOException
+     */
+    public static void deleteRowKey(String tableName, String rowKey, String cf, String cn) throws IOException {
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        //删除rowkey
+        Delete deleteRowKey = new Delete(Bytes.toBytes(rowKey));
+        // type标记 DeleteFamily，删除整个列族
+        table.delete(deleteRowKey);
+
+        //删除cf
+        Delete deleteCf = new Delete(Bytes.toBytes(rowKey))
+                .addFamily(Bytes.toBytes(cf));
+        //type标记 DeleteFamily，删除整个列族
+        table.delete(deleteCf);
+
+        //fixme 删除cn，删除单个版本,其他版本的数据会显示，慎用
+        Delete delete = new Delete(Bytes.toBytes(rowKey))
+                .addColumn(Bytes.toBytes(cf), Bytes.toBytes(cn));
+
+        // 删除对应版本数据，type为Delete，只删除当前时间戳
+        Delete deletets = new Delete(Bytes.toBytes(rowKey))
+                .addColumn(Bytes.toBytes(cf), Bytes.toBytes(cn),123421512555L);
+
+        //type标记 Delete
+        table.delete(delete);
+
+        // 删除cn，删除多个版本，其他版本数据不会显示
+        Delete deletecn = new Delete(Bytes.toBytes(rowKey))
+                .addColumns(Bytes.toBytes(cf), Bytes.toBytes(cn));
+        //type标记 DeleteColumn，删除整个column
+        table.delete(deletecn);
+
     }
 
     public static void main(String[] args) throws Exception {
