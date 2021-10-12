0b52980607203b8399e7f65744ccc55c270a4788
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-10-12 15:30:02 +0800

    rocksdb

---
+++ rocksdb-learning/src/main/java/org/rocksdb/db/BlobDB.java
@@ -0,0 +1,13 @@
+package org.rocksdb.db;
+
+/**
+ * @fileName: BlobDB.java
+ * @description: BlobDB.java类说明
+ * @author: huangshimin
+ * @date: 2021/10/12 3:16 下午
+ */
+public class BlobDB {
+    public static void main(String[] args) {
+
+    }
+}
--- rocksdb-learning/src/main/java/org/rocksdb/options/OptionsFeature.java
+++ rocksdb-learning/src/main/java/org/rocksdb/options/OptionsFeature.java
@@ -1,8 +1,10 @@
 package org.rocksdb.options;
 
 
+import org.rocksdb.BuiltinComparator;
 import org.rocksdb.FlushOptions;
 import org.rocksdb.Options;
+import org.rocksdb.ReadOptions;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 import org.rocksdb.WriteOptions;
@@ -19,9 +21,18 @@ public class OptionsFeature {
         Options options = new Options();
         // 多个列族原子性flush
         options.setAtomicFlush(true);
+        // 设置比较器
+        options.setComparator(BuiltinComparator.BYTEWISE_COMPARATOR);
 
         RocksDB db = RocksDB.open("test");
         FlushOptions flushOptions = new FlushOptions();
         db.flush(flushOptions);
+    }
+
+    private void tsOpt() throws RocksDBException {
+        WriteOptions writeOptions = new WriteOptions();
+        ReadOptions readOptions = new ReadOptions();
+        RocksDB db = RocksDB.open("test");
+        db.get(readOptions, "test".getBytes());
     }
 }
