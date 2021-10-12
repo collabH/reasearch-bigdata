8052728d30f1a8857af8e257cca9c20a85a963df
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-10-12 21:22:04 +0800

    feat：rocksdb options

--- rocksdb-learning/src/main/java/org/rocksdb/options/OptionsFeature.java
+++ rocksdb-learning/src/main/java/org/rocksdb/options/OptionsFeature.java
@@ -1,13 +1,22 @@
 package org.rocksdb.options;
 
 
+import org.rocksdb.BlockBasedTableConfig;
 import org.rocksdb.BuiltinComparator;
+import org.rocksdb.Cache;
+import org.rocksdb.ColumnFamilyDescriptor;
+import org.rocksdb.ColumnFamilyHandle;
+import org.rocksdb.ColumnFamilyOptions;
+import org.rocksdb.DBOptions;
 import org.rocksdb.FlushOptions;
 import org.rocksdb.Options;
 import org.rocksdb.ReadOptions;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 import org.rocksdb.WriteOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @fileName: OptionsFeature.java
@@ -35,4 +44,33 @@ public class OptionsFeature {
         RocksDB db = RocksDB.open("test");
         db.get(readOptions, "test".getBytes());
     }
+
+
+    private void writeBufferSizeOptions() throws Exception {
+        DBOptions dbOptions = new DBOptions();
+        // 默认大小是关闭的
+        dbOptions.setDbWriteBufferSize(64 << 30);
+        List<ColumnFamilyDescriptor> columnFamilyDescriptors = new ArrayList<>();
+        ColumnFamilyOptions columnFamilyOptions = new ColumnFamilyOptions();
+        // 默认为64MB
+        columnFamilyOptions.setWriteBufferSize(64 << 20);
+        ColumnFamilyDescriptor columnFamilyDescriptor = new ColumnFamilyDescriptor("name".getBytes(),
+                columnFamilyOptions);
+        columnFamilyDescriptors.add(columnFamilyDescriptor);
+        List<ColumnFamilyHandle> columnFamilyHandles = new ArrayList<>();
+        RocksDB.open(dbOptions, "test", columnFamilyDescriptors, columnFamilyHandles);
+    }
+
+    private void blockSizeOptions() throws Exception {
+        BlockBasedTableConfig blockBasedTableConfig = new BlockBasedTableConfig();
+        // 设置128MB
+        blockBasedTableConfig.setBlockCache(new Cache(128 << 20) {
+            @Override
+            protected void disposeInternal(long handle) {
+
+            }
+        });
+
+    }
+
 }
