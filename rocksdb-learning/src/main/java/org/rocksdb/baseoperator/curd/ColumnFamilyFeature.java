a6a882cd7c327288e41538889cd55a2bef555763
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-10-21 17:17:15 +0800

    feat(rocksdb): tableConfig配置

--- rocksdb-learning/src/main/java/org/rocksdb/baseoperator/curd/ColumnFamilyFeature.java
+++ rocksdb-learning/src/main/java/org/rocksdb/baseoperator/curd/ColumnFamilyFeature.java
@@ -2,12 +2,18 @@ package org.rocksdb.baseoperator.curd;
 
 import com.google.common.collect.Lists;
 import org.rocksdb.BlockBasedTableConfig;
+import org.rocksdb.BloomFilter;
+import org.rocksdb.ChecksumType;
 import org.rocksdb.ColumnFamilyDescriptor;
 import org.rocksdb.ColumnFamilyHandle;
 import org.rocksdb.ColumnFamilyOptions;
 import org.rocksdb.CompactionStyle;
 import org.rocksdb.DBOptions;
+import org.rocksdb.Filter;
+import org.rocksdb.HdfsEnv;
+import org.rocksdb.IndexType;
 import org.rocksdb.LRUCache;
+import org.rocksdb.PersistentCache;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 import org.rocksdb.RocksIterator;
@@ -44,6 +50,29 @@ public class ColumnFamilyFeature {
         blockBasedTableConfig.setMetadataBlockSize(4 << 10);
         // 设置cache过期策略
         blockBasedTableConfig.setBlockCache(new LRUCache(100));
+        blockBasedTableConfig.setChecksumType(ChecksumType.kCRC32c);
+        // 检索block时判断key是否存在
+        blockBasedTableConfig.setFilterPolicy(new BloomFilter());
+        // 开启索引压缩
+        blockBasedTableConfig.setEnableIndexCompression(true);
+        /**
+         * 1.kHashSearch:（如果启用）将在提供Options.prefix_extractor时进行哈希查找
+         * 2.kTwoLevelIndexSearch。 两个级别都是二分搜索索引。
+         * 3.kBinarySearch
+         * 4.kBinarySearchWithFirstKey，像kBinarySearch ，但 index 也包含每个块的第一个键。 这允许迭代器推迟读取块，直到真正需要它。
+         * 可能会显着减少短程扫描的读取放大。 没有它，迭代器搜索通常从每个 0 级文件和每个级别读取一个块，这可能很昂贵。 结合使用效果最佳： - IndexShorteningMode::kNoShortening， - 自定义 FlushBlockPolicy 在某些有意义的边界处切割块，例如当前缀更改时。 使索引明显更大（2 倍或更多），尤其是当键很长时
+         */
+        blockBasedTableConfig.setIndexType(IndexType.kTwoLevelIndexSearch);
+        // 对每个 SST 文件使用分区的完整过滤器。 此选项与基于block不兼容。 默认为flase。
+        blockBasedTableConfig.setPartitionFilters(true);
+//        blockBasedTableConfig.setPersistentCache(new PersistentCache(new HdfsEnv("test"),path,100))
+        blockBasedTableConfig.setPinTopLevelIndexAndFilter(true);
+        // 示我们是否将索引/过滤器块放入块缓存。
+        blockBasedTableConfig.setCacheIndexAndFilterBlocks(true);
+        // index和filter是否高优先级
+        blockBasedTableConfig.setCacheIndexAndFilterBlocksWithHighPriority(true);
+        // 指示我们是否要将 L0 索引/过滤器块固定到块缓存。 如果未指定，则默认为 false。
+        blockBasedTableConfig.setPinL0FilterAndIndexBlocksInCache(true);
         columnFamilyOptions.setTableFormatConfig(blockBasedTableConfig);
         ColumnFamilyDescriptor defaultCf = new ColumnFamilyDescriptor(RocksDB.DEFAULT_COLUMN_FAMILY);
         ColumnFamilyDescriptor userCf = new ColumnFamilyDescriptor("user".getBytes(), columnFamilyOptions);
