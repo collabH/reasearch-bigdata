1498c479624fb926d8d8841cd4645a62c579847a
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-10-28 17:21:57 +0800

    Update RocksDBValueParser.java

--- rocksdb-learning/src/main/java/org/rocksdb/utils/RocksDBValueParser.java
+++ rocksdb-learning/src/main/java/org/rocksdb/utils/RocksDBValueParser.java
@@ -1,6 +1,10 @@
 package org.rocksdb.utils;
 
+import com.google.common.collect.Lists;
+
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
 /**
@@ -14,5 +18,11 @@ public class RocksDBValueParser implements Function<byte[], String> {
     @Override
     public String apply(byte[] values) {
         return new String(values, Charset.defaultCharset());
+    }
+
+    public static void main(String[] args) {
+        List<Integer> potentialSubCustomerDataList = Lists.newArrayList(1, 23, 4, 535, 23, 213, 421, 1);
+        List<List<Integer>> partitionData = Lists.partition(potentialSubCustomerDataList, 3);
+        System.out.println(partitionData);
     }
 }
